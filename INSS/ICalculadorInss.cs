94d9455d83457190f7016fa922d6f705e7d79b1f
NogueiraJr <nogueirajr@hotmail.com>
2019-08-09 22:11:52 -0300

    Add files via upload

---
+++ INSS/ICalculadorInss.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSS
+{
+    public interface ICalculadorInss
+	{
+		/// <summary>
+		/// Deve retornar o deconto do INSS aplicado ao salário, na determinada data.
+		/// </summary>
+		decimal CalcularDesconto(DateTime data, decimal salario);
+    }
+
+    public class clsInss : ICalculadorInss {
+        public decimal CalcularDesconto(DateTime data, decimal salario) {
+            decimal[,] tbl = getTable();
+
+            var r = 0M;
+            for (int x = 0; x < tbl.GetLength(0); x++) {
+                if (tbl[x, 0] == data.Year) {
+                    if (salario >= tbl[x, 1] && salario <= tbl[x, 2]) {
+                        r = salario * (tbl[x, 3] / 100);
+                        if (r > 0) return r;
+                    }
+                } else r = tbl[x, 4];
+            } return r;
+        }
+
+        /// <summary>
+        /// Gets the table.
+        /// </summary>
+        /// <returns>Return the table static or dinamic for External Database.</returns>
+        private static decimal[,] getTable() {
+            return new decimal[,] { /* sempre o teto do ano sub-sequente */
+                                  { 2011,0000000M,1106.90M,08.00M,000.00M }
+                                , { 2011,1106.91M,1844.83M,09.00M,000.00M }
+                                , { 2011,1844.84M,3689.66M,11.00M,500.00M }
+
+                                , { 2012,0000000M,1000.00M,07.00M,000.00M }
+                                , { 2012,1000.01M,1500.00M,08.00M,000.00M }
+                                , { 2012,1500.01M,3000.00M,09.00M,000.00M }
+                                , { 2012,3000.01M,4000.00M,11.00M,405.86M }
+            };
+        }
+    }
+}
---
+++ INSS/zip/InssTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InssTest {
+    class MainClass {
+        public static void Main() {
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2011"), 1000.00M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2011"), 1100.00M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2011"), 1102.00M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2011"), 1250.00M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2011"), 2857.00M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2011"), 2857.03M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2011"), 4000.99M));
+            Console.WriteLine("");
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2012"), 1000.00M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2012"), 1127.54M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2012"), 1503.78M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2012"), 3058.23M));
+            Console.WriteLine((new INSS.clsInss()).CalcularDesconto(Convert.ToDateTime("01/01/2012"), 5000.99M));
+
+            //Console.ReadLine(); /* se Windows */
+        }
+    }
+}
